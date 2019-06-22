d147e8a3e8cbbc86e6c4f6e07a7fbf19c1b7f0ee
shaoaz <907024853@qq.com>
2019-06-22 15:10:50 +0800

    Merge branch 'master' of https://github.com/shaoaz/mylearning
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/liang/haha/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.liang.haha;
+
+/**
+ * Created by Administrator on 2019/6/22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("nnn");
+    }
+}
